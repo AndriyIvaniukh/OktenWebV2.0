3dd4114dfd99d47355942f9f4b5cd63cff8ffd01
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-02-08 14:21:36 +0200

    start react lesson5 hw

--- React/lesson3_hw/src/App.js
+++ React/lesson3_hw/src/App.js
@@ -1,44 +1,11 @@
-import {Routes, Route, Link} from "react-router-dom";
 import React from 'react';
-
-import UsersPage from "./pages/UsersPage/usersPage";
-import PostsPage from "./pages/PostsPage/postsPage";
-import "./app.css";
-import PostDetails from "./pages/PostDetails/PostDetails";
-import Comments from "./pages/Comments/Comments";
-import UserDetails from "./pages/UserDetails/UserDetails";
-import UserPosts from "./pages/UserPosts/UserPosts";
-import UserAlbums from "./pages/UserAlbums/UserAlbums";
-import PhotosByAlbum from "./pages/PhotosByAlbum/PhotosByAlbum";
-
 
 const App = () => {
     return (
-        <div className={'app'}>
-            <div className="header">
-                <Link to='/users'>Users</Link>
-                <Link to='/posts'>Posts</Link>
-            </div>
-            <div className="page">
-                <Routes>
-                    <Route path={'/users'} element={<UsersPage/>}>
-                        <Route path={':id'} element={<UserDetails/>}>
-                            <Route path={'userPosts'} element={<UserPosts/>}/>
-                        </Route>
-                        <Route path={':id/albums'} element={<UserAlbums/>}>
-                            <Route path={'photos/albumId=:albumId'} element={<PhotosByAlbum/>}/>
-                        </Route>
-                    </Route>
-                    <Route path={'/posts'} element={<PostsPage/>}>
-                        <Route path={':id'} element={<PostDetails/>}>
-                            <Route path={'comments'} element={<Comments/>}/>
-                        </Route>
-                    </Route>
-                </Routes>
-
-            </div>
+        <div>
+            
         </div>
-    )
+    );
 };
 
 export default App;
--- React/lesson3_hw/src/services/photos.service.js
+++
@@ -1,8 +0,0 @@
-import {axiosService} from "./axios.service";
-import {urls} from "../configs/urls";
-
-
-export const photosService = {
-    getAll: () => axiosService.get(urls.photos).then(value => value.data),
-    getByAlbumsID: (albumId) =>  axiosService.get(`${urls.photos}?albumId=${albumId}`).then(value => value.data)
-}
