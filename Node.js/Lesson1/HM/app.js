2a26879f06c35683d988ac1654b6824c8d039d66
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-02-20 11:08:23 +0200

    Node.js lesson1 HM without mistakes

--- Node.js/Lesson1/HM/app.js
+++ Node.js/Lesson1/HM/app.js
@@ -1,26 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'main'), (err) => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-});
-
-fs.mkdir(path.join(__dirname, 'main', 'online'), err => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-});
-
-fs.mkdir(path.join(__dirname, 'main', 'inPerson'), err => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-});
 
 const onLineUsers = [
     {name: 'Andriy', age: 26, city: 'Lviv'},
@@ -38,44 +18,52 @@ const inPersonUsers = [
     {name: 'Petro', age: 20, city: 'Krakow'},
 ];
 
-for (const onLineUser of onLineUsers) {
-    fs.appendFile(path.join(__dirname, 'main', 'online', 'onLineUser.txt'),
-        `Name: ${onLineUser.name} \nAge: ${onLineUser.age} \nCity: ${onLineUser.city} \n\n`,
-        err => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-        }
-    )
-}
 
-for (const inPersonUser of inPersonUsers) {
-    fs.appendFile(path.join(__dirname, 'main', 'inPerson', 'inPersonUser.txt'),
-        `Name: ${inPersonUser.name} \nAge: ${inPersonUser.age} \nCity: ${inPersonUser.city} \n\n`,
-        err => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-        }
-    )
-}
-
-fs.rename(path.join(__dirname, 'main', 'inPerson', 'inPersonUser.txt'),
-    path.join(__dirname, 'main', 'online', 'inPersonUser.txt'),
-    (err) => {
+fs.mkdir(path.join(__dirname, 'main'), (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    fs.mkdir(path.join(__dirname, 'main', 'online'), err => {
         if (err) {
             console.log(err);
             throw err;
         }
+        addToFile(onLineUsers, 'online', 'onLineUsers.txt');
+        moveToOtherFoolder('online', 'inPerson', 'onLineUsers.txt')
     });
 
-fs.rename(path.join(__dirname, 'main', 'online', 'onLineUser.txt'),
-    path.join(__dirname, 'main', 'inPerson', 'onLineUser.txt'),
-    (err) => {
+    fs.mkdir(path.join(__dirname, 'main', 'inPerson'), err => {
         if (err) {
             console.log(err);
             throw err;
         }
+        addToFile(inPersonUsers, 'inPerson', 'inPersonUsers.txt');
+        moveToOtherFoolder('inPerson', 'online', 'inPersonUsers.txt')
+    });
 });
+
+function moveToOtherFoolder(oldWay, newWay, fileName) {
+    fs.rename(path.join(__dirname, 'main', oldWay, fileName),
+        path.join(__dirname, 'main', newWay, fileName),
+        (err) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+}
+
+function addToFile(arrayOfUsers, foolderName, fileName) {
+    for (const user of arrayOfUsers) {
+        fs.appendFile(path.join(__dirname, 'main', foolderName, fileName),
+            `Name: ${user.name} \nAge: ${user.age} \nCity: ${user.city} \n\n`,
+            err => {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+            }
+        )
+    }
+}
